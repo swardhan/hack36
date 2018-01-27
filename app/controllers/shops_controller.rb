54d05812f1060448dc7845f2b868a283087cb9c7
saumya <saumyawardhan1@gmail.com>
2018-01-28 02:28:48 +0530

    Add Closes Shop

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -45,6 +45,36 @@ class ShopsController < ApplicationController
       render :json => data
   end
 
+  def closest_shop
+    user_latitude = params[:latitude]
+    user_longitude = params[:longitude]
+    shops = Shop.all
+    least_distance = 100000000
+    current_distance = 0
+    nearest_shop = Shop.new
+    shops.each_with_index do |shop, index|
+      puts 1
+      current_distance = calculate(user_latitude, user_longitude, shop.lon, shop.lat)
+      if(current_distance < least_distance)
+        least_distance = current_distance
+        puts least_distance
+        nearest_shop = shop
+      end
+    end
+
+    data = {
+      name: nearest_shop.name,
+      address: nearest_shop.address,
+      cordinates: {
+        longitude: nearest_shop.lon,
+        latitude: nearest_shop.lat,
+      },
+      medicines: nearest_shop.medicines
+    }
+
+    render :json => data
+  end
+
   private
   def calculate(friend_latitude, friend_longitude, office_latitude, office_longitude)
     office_location = Geokit::LatLng.new(office_latitude, office_longitude)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'shops/nearest'
 
-  get '/nearest', to: "shops#nearest" 
+  get 'shops/nearest'
+  get '/nearest', to: "shops#nearest"
+  get '/closest_shop', to: "shops#closest_shop"
+
 end
